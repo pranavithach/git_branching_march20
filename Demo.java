b841e9bceea6da03f416998a0dd703fc58167001
pranavitha <pranavithapranavitha@gmail.com>
2024-03-20 12:48:31 +0530

    Modified java&text files

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 class Demo{
     public static void main(string [] args){
         system.out.println("Hello all");
+        system.out.println("Welcome to basic of DevOps");
     }
 }  
--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
 Hello iam pranavitha!!!!!
 Iam interested to learn devops
+Welcome to Git session
